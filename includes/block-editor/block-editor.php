a9f8e22492879c91ea96dea93beeb08e3d6e33a6
Takayuki Miyoshi <takayukister@gmail.com>
2020-09-17 17:22:48 +0900

    Restructure block editor assets

---
+++ includes/block-editor/block-editor.php
@@ -0,0 +1,45 @@
+<?php
+
+add_action( 'init', 'wpcf7_init_block_editor_assets', 10, 0 );
+
+function wpcf7_init_block_editor_assets() {
+	$asset_file = wpcf7_plugin_path(
+		'includes/block-editor/block-editor.asset.php'
+	);
+
+	if ( file_exists( $asset_file ) ) {
+		$assets = include( $asset_file );
+	} else {
+		$assets = null;
+	}
+
+	$assets = wp_parse_args( $assets, array(
+		'src' => wpcf7_plugin_url( 'includes/block-editor/block-editor.js' ),
+		'dependencies' => array(
+			'wp-blocks',
+			'wp-element',
+			'wp-i18n',
+			'wp-polyfill',
+		),
+		'version' => WPCF7_VERSION,
+	) );
+
+	wp_register_script(
+		'contact-form-7-block-editor',
+		$assets['src'],
+		$assets['dependencies'],
+		$assets['version'],
+	);
+
+	wp_set_script_translations(
+		'contact-form-7-block-editor',
+		'contact-form-7'
+	);
+
+	register_block_type(
+		'contact-form-7/contact-form-selector',
+		array(
+			'editor_script' => 'contact-form-7-block-editor',
+		)
+	);
+}
