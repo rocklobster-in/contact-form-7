56b9a6c197712d482971aa3dd821399fa3d83a71
Takayuki Miyoshi <takayukister@gmail.com>
2020-09-25 19:23:11 +0900

    Delete index.asset.php

--- includes/block-editor/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f7629b46ba025f436cdab719d4c1bd9e');
