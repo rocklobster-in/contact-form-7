d2db74b7c50efe5d6904ccbd24a8ff99c44cd0b1
Takayuki Miyoshi <takayukister@gmail.com>
2020-10-07 19:17:15 +0900

    Use "disallowed list" instead of "comment blacklist"
    See #170

--- admin/includes/welcome-panel.php
+++ admin/includes/welcome-panel.php
@@ -25,7 +25,7 @@ function wpcf7_welcome_panel() {
 
 				<p><?php
 	echo sprintf(
-		/* translators: links labeled 1: 'Akismet', 2: 'reCAPTCHA', 3: 'comment blacklist' */
+		/* translators: links labeled 1: 'Akismet', 2: 'reCAPTCHA', 3: 'disallowed list' */
 		esc_html( __( 'Contact Form 7 supports spam-filtering with %1$s. Intelligent %2$s blocks annoying spambots. Plus, using %3$s, you can block messages containing specified keywords or those sent from specified IP addresses.', 'contact-form-7' ) ),
 		wpcf7_link(
 			__( 'https://contactform7.com/spam-filtering-with-akismet/', 'contact-form-7' ),
@@ -37,7 +37,7 @@ function wpcf7_welcome_panel() {
 		),
 		wpcf7_link(
 			__( 'https://contactform7.com/comment-blacklist/', 'contact-form-7' ),
-			__( 'comment blacklist', 'contact-form-7' )
+			__( 'disallowed list', 'contact-form-7' )
 		)
 	);
 				?></p>
