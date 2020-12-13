4537f4d840f6fd27b40945fee56614fd7a9f1d31
Takayuki Miyoshi <takayukister@gmail.com>
2020-12-13 21:03:55 +0900

    Set status based on the response
    See #227

--- includes/js/src/submit.js
+++ includes/js/src/submit.js
@@ -30,5 +30,43 @@ export default function submit( form ) {
 		method: 'POST',
 		body: formData,
 	} ).then( response => {
+		detail.id = document.querySelector( response.into ).getAttribute( 'id' );
+		detail.status = response.status;
+		detail.apiResponse = response;
+
+		switch ( response.status ) {
+			case 'init':
+				wpcf7.setStatus( form, 'init' );
+				break;
+
+			case 'validation_failed':
+				wpcf7.setStatus( form, 'invalid' );
+				break;
+
+			case 'acceptance_missing':
+				wpcf7.setStatus( form, 'unaccepted' );
+				break;
+
+			case 'spam':
+				wpcf7.setStatus( form, 'spam' );
+				break;
+
+			case 'aborted':
+				wpcf7.setStatus( form, 'aborted' );
+				break;
+
+			case 'mail_sent':
+				wpcf7.setStatus( form, 'sent' );
+				break;
+
+			case 'mail_failed':
+				wpcf7.setStatus( form, 'failed' );
+				break;
+
+			default:
+				wpcf7.setStatus( form,
+					`custom-${ response.status.replace( /[^0-9a-z]+/i, '-' ) }`
+				);
+		}
 	} );
 }
