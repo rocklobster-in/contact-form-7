60840fae246863275b25e63f02948df480d9d0a9
Takayuki Miyoshi <takayukister@gmail.com>
2020-12-14 13:34:50 +0900

    Define defaultStatuses
    See #227

--- includes/js/src/submit.js
+++ includes/js/src/submit.js
@@ -8,11 +8,10 @@ export default function submit( form ) {
 	const formData = new FormData( form );
 
 	const detail = {
-		id: form.wpcf7.unitTag,
-		unitTag: form.wpcf7.unitTag,
 		contactFormId: form.wpcf7.id,
 		pluginVersion: form.wpcf7.pluginVersion,
 		contactFormLocale: form.wpcf7.locale,
+		unitTag: form.wpcf7.unitTag,
 		containerPostId: form.wpcf7.containerPost,
 		status: form.wpcf7.status,
 		inputs: Array.from(
@@ -30,43 +29,9 @@ export default function submit( form ) {
 		method: 'POST',
 		body: formData,
 	} ).then( response => {
-		detail.id = document.querySelector( response.into ).getAttribute( 'id' );
 		detail.status = response.status;
 		detail.apiResponse = response;
 
-		switch ( response.status ) {
-			case 'init':
-				wpcf7.setStatus( form, 'init' );
-				break;
-
-			case 'validation_failed':
-				wpcf7.setStatus( form, 'invalid' );
-				break;
-
-			case 'acceptance_missing':
-				wpcf7.setStatus( form, 'unaccepted' );
-				break;
-
-			case 'spam':
-				wpcf7.setStatus( form, 'spam' );
-				break;
-
-			case 'aborted':
-				wpcf7.setStatus( form, 'aborted' );
-				break;
-
-			case 'mail_sent':
-				wpcf7.setStatus( form, 'sent' );
-				break;
-
-			case 'mail_failed':
-				wpcf7.setStatus( form, 'failed' );
-				break;
-
-			default:
-				wpcf7.setStatus( form,
-					`custom-${ response.status.replace( /[^0-9a-z]+/i, '-' ) }`
-				);
-		}
+		wpcf7.setStatus( form, response.status );
 	} );
 }
