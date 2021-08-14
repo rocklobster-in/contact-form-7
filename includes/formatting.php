d04057e461f13ea3310df9b5dade6c4eab3e6174
Takayuki Miyoshi <takayukister@gmail.com>
2021-08-14 20:18:30 +0900

    Apply KSES filter to form and email body
    #492

--- includes/formatting.php
+++ includes/formatting.php
@@ -252,3 +252,173 @@ function wpcf7_mask_password( $text, $right = 0, $left = 0 ) {
 
 	return $text;
 }
+
+
+/**
+ * Returns an array of allowed HTML tags and attributes for a given context.
+ *
+ * @param string $context Context used to decide allowed tags and attributes.
+ * @return array Array of allowed HTML tags and their allowed attributes.
+ */
+function wpcf7_kses_allowed_html( $context = 'form' ) {
+	static $allowed_tags = array();
+
+	if ( isset( $allowed_tags[$context] ) ) {
+		return apply_filters(
+			'wpcf7_kses_allowed_html',
+			$allowed_tags[$context],
+			$context
+		);
+	}
+
+	$allowed_tags[$context] = wp_kses_allowed_html( 'post' );
+
+	if ( 'form' === $context ) {
+		$additional_tags_for_form = array(
+			'button' => array(
+				'autofocus' => true,
+				'disabled' => true,
+				'name' => true,
+				'type' => true,
+				'value' => true,
+			),
+			'datalist' => array(),
+			'fieldset' => array(
+				'disabled' => true,
+				'name' => true,
+			),
+			'input' => array(
+				'accept' => true,
+				'alt' => true,
+				'autofocus' => true,
+				'capture' => true,
+				'checked' => true,
+				'disabled' => true,
+				'list' => true,
+				'max' => true,
+				'maxlength' => true,
+				'min' => true,
+				'minlength' => true,
+				'multiple' => true,
+				'name' => true,
+				'placeholder' => true,
+				'readonly' => true,
+				'size' => true,
+				'step' => true,
+				'type' => true,
+				'value' => true,
+			),
+			'label' => array(
+				'for' => true,
+			),
+			'legend' => array(),
+			'meter' => array(
+				'value' => true,
+				'min' => true,
+				'max' => true,
+				'low' => true,
+				'high' => true,
+				'optimum' => true,
+			),
+			'optgroup' => array(
+				'disabled' => true,
+				'label' => true,
+			),
+			'option' => array(
+				'disabled' => true,
+				'label' => true,
+				'selected' => true,
+				'value' => true,
+			),
+			'output' => array(
+				'for' => true,
+				'name' => true,
+			),
+			'progress' => array(
+				'max' => true,
+				'value' => true,
+			),
+			'select' => array(
+				'autofocus' => true,
+				'disabled' => true,
+				'multiple' => true,
+				'name' => true,
+				'size' => true,
+			),
+			'textarea' => array(
+				'autofocus' => true,
+				'cols' => true,
+				'disabled' => true,
+				'maxlength' => true,
+				'minlength' => true,
+				'name' => true,
+				'placeholder' => true,
+				'readonly' => true,
+				'rows' => true,
+				'spellcheck' => true,
+				'wrap' => true,
+			),
+		);
+
+		$additional_tags_for_form = array_map(
+			function ( $elm ) {
+				$global_attributes = array(
+					'aria-atomic' => true,
+					'aria-checked' => true,
+					'aria-describedby' => true,
+					'aria-details' => true,
+					'aria-disabled' => true,
+					'aria-hidden' => true,
+					'aria-invalid' => true,
+					'aria-label' => true,
+					'aria-labelledby' => true,
+					'aria-live' => true,
+					'aria-relevant' => true,
+					'aria-required' => true,
+					'aria-selected' => true,
+					'class' => true,
+					'data-*' => true,
+					'id' => true,
+					'inputmode' => true,
+					'role' => true,
+					'style' => true,
+					'tabindex' => true,
+					'title' => true,
+				);
+
+				return array_merge( $global_attributes, (array) $elm );
+			},
+			$additional_tags_for_form
+		);
+
+		$allowed_tags[$context] = array_merge(
+			$allowed_tags[$context],
+			$additional_tags_for_form
+		);
+	}
+
+	return apply_filters(
+		'wpcf7_kses_allowed_html',
+		$allowed_tags[$context],
+		$context
+	);
+}
+
+
+/**
+ * Sanitizes content for allowed HTML tags for the specified context.
+ *
+ * @param string $input Content to filter. Slashed text is expected.
+ * @param string $context Context used to decide allowed tags and attributes.
+ * @return string Filtered text with allowed HTML tags and attributes intact.
+ */
+function wpcf7_kses( $input, $context = 'form' ) {
+	$input = stripslashes( $input );
+
+	$output = wp_kses(
+		$input,
+		wpcf7_kses_allowed_html( $context )
+	);
+
+	return addslashes( $output );
+}
