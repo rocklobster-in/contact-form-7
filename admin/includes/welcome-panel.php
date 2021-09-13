ba48910a2530e0afb947160e66d4441459a67a86
Takayuki Miyoshi <takayukister@gmail.com>
2021-09-13 21:08:27 +0900

    Update welcome-panel.php
    #23

--- admin/includes/welcome-panel.php
+++ admin/includes/welcome-panel.php
@@ -4,7 +4,8 @@ function wpcf7_welcome_panel() {
 	$classes = 'welcome-panel';
 
 	$vers = (array) get_user_meta( get_current_user_id(),
-		'wpcf7_hide_welcome_panel_on', true );
+		'wpcf7_hide_welcome_panel_on', true
+	);
 
 	if ( wpcf7_version_grep( wpcf7_version( 'only_major=1' ), $vers ) ) {
 		$classes .= ' hidden';
@@ -87,14 +88,19 @@ function wpcf7_welcome_panel() {
 <?php
 }
 
-add_action( 'wp_ajax_wpcf7-update-welcome-panel',
-	'wpcf7_admin_ajax_welcome_panel', 10, 0 );
+
+add_action(
+	'wp_ajax_wpcf7-update-welcome-panel',
+	'wpcf7_admin_ajax_welcome_panel',
+	10, 0
+);
 
 function wpcf7_admin_ajax_welcome_panel() {
 	check_ajax_referer( 'wpcf7-welcome-panel-nonce', 'welcomepanelnonce' );
 
 	$vers = get_user_meta( get_current_user_id(),
-		'wpcf7_hide_welcome_panel_on', true );
+		'wpcf7_hide_welcome_panel_on', true
+	);
 
 	if ( empty( $vers ) or ! is_array( $vers ) ) {
 		$vers = array();
@@ -107,7 +113,8 @@ function wpcf7_admin_ajax_welcome_panel() {
 	$vers = array_unique( $vers );
 
 	update_user_meta( get_current_user_id(),
-		'wpcf7_hide_welcome_panel_on', $vers );
+		'wpcf7_hide_welcome_panel_on', $vers
+	);
 
 	wp_die( 1 );
 }
