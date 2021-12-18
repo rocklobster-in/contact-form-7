db5f2ab836d41b20912acc7a7efb9ea172f57f39
Takayuki Miyoshi <takayukister@gmail.com>
2021-12-18 14:25:33 +0900

    New additional setting: doi
    #644

---
+++ includes/doi.php
@@ -0,0 +1,12 @@
+<?php
+
+function wpcf7_do_doi( $contact_form, $args = '' ) {
+	$do_doi = ! array_filter(
+		$contact_form->additional_setting( 'doi', false ),
+		function ( $setting ) {
+			return in_array( $setting, array( 'off', 'false', '0' ), true );
+		}
+	);
+
+	return apply_filters( 'wpcf7_do_doi', $do_doi, 'constant_contact', $args );
+}
