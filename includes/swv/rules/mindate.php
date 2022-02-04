7c4ff5e198ebb1f90e318d1ddef7d75e1d38690b
Takayuki Miyoshi <takayukister@gmail.com>
2022-02-04 17:54:27 +0900

    Implement mindate and maxdate rules
    #679

--- includes/swv/rules/mindate.php
+++ includes/swv/rules/mindate.php
@@ -4,7 +4,39 @@ class WPCF7_SWV_MinDateRule extends WPCF7_SWV_Rule {
 
 	const rule_name = 'mindate';
 
+	public function match( $context ) {
+		if ( false === parent::match( $context ) ) {
+			return false;
+		}
+
+		if ( empty( $context['text'] ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public function validate( $context ) {
+		$field = $this->get_property( 'field' );
+		$input = isset( $_POST[$field] ) ? $_POST[$field] : '';
+		$input = wpcf7_array_flatten( $input );
+		$input = wpcf7_exclude_blank( $input );
+
+		$threshold = $this->get_property( 'threshold' );
+
+		if ( ! wpcf7_is_date( $threshold ) ) {
+			return true;
+		}
+
+		foreach ( $input as $i ) {
+			if ( wpcf7_is_date( $i ) and $i < $threshold ) {
+				return new WP_Error( 'wpcf7_invalid_mindate',
+					$this->get_property( 'message' )
+				);
+			}
+		}
+
+		return true;
 	}
 
 	public function to_array() {
