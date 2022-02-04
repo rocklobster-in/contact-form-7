4101e43d7e37bb919bf0033fc52cc3fbc6f7c3fd
Takayuki Miyoshi <takayukister@gmail.com>
2022-02-04 19:05:50 +0900

    Implement file and maxfilesize rules
    #679

--- includes/swv/rules/file.php
+++ includes/swv/rules/file.php
@@ -4,7 +4,47 @@ class WPCF7_SWV_FileRule extends WPCF7_SWV_Rule {
 
 	const rule_name = 'file';
 
+	public function match( $context ) {
+		if ( false === parent::match( $context ) ) {
+			return false;
+		}
+
+		if ( empty( $context['file'] ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public function validate( $context ) {
+		$field = $this->get_property( 'field' );
+		$input = isset( $_FILES[$field]['name'] ) ? $_FILES[$field]['name'] : '';
+		$input = wpcf7_array_flatten( $input );
+		$input = wpcf7_exclude_blank( $input );
+
+		$accept = (array) $this->get_property( 'accept' );
+
+		// TODO: Support MIME type strings in $accept
+
+		foreach ( $input as $i ) {
+			$last_period_pos = strrpos( $i, '.' );
+
+			if ( false === $last_period_pos ) { // no period
+				return new WP_Error( 'wpcf7_invalid_file',
+					$this->get_property( 'message' )
+				);
+			}
+
+			$suffix = substr( $i, $last_period_pos );
+
+			if ( ! in_array( $suffix, $accept, true ) ) {
+				return new WP_Error( 'wpcf7_invalid_file',
+					$this->get_property( 'message' )
+				);
+			}
+		}
+
+		return true;
 	}
 
 	public function to_array() {
