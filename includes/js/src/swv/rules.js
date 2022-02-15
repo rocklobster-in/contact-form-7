4fc5051ad9141c26153427bed2c8683761ab1994
Takayuki Miyoshi <takayukister@gmail.com>
2022-02-15 19:32:50 +0900

    Move utility functions to utils.js
    #679

--- includes/js/src/swv/rules.js
+++ includes/js/src/swv/rules.js
@@ -1,19 +1,5 @@
+import { getFieldValues } from './utils';
 import { ValidationError } from './error';
-
-function getFieldValues( formData, fieldName ) {
-	let values = [];
-
-	for ( const field of formData.entries() ) {
-		const name = field[ 0 ];
-		const value = field[ 1 ];
-
-		if ( fieldName === name && '' !== value ) {
-			values.push( value );
-		}
-	}
-
-	return values;
-}
 
 export const required = function ( formData ) {
 	const values = getFieldValues( formData, this.field );
---
+++ includes/js/src/swv/utils.js
@@ -0,0 +1,14 @@
+export function getFieldValues( formData, fieldName ) {
+	let values = [];
+
+	for ( const field of formData.entries() ) {
+		const name = field[ 0 ].replace( /\[.*\]$/, '' );
+		const value = field[ 1 ];
+
+		if ( fieldName === name && '' !== value ) {
+			values.push( value );
+		}
+	}
+
+	return values;
+}
