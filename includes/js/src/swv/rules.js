591bdd6c9ec2ef0a9313a50432ee784000817f1d
Takayuki Miyoshi <takayukister@gmail.com>
2022-03-17 18:22:07 +0900

    email rule uses is_email() equivalent by default
    #679

--- includes/js/src/swv/rules.js
+++ includes/js/src/swv/rules.js
@@ -31,10 +31,57 @@ export const requiredfile = function ( formDataTree ) {
 export const email = function ( formDataTree ) {
 	const values = formDataTree.getAll( this.field );
 
-	// https://html.spec.whatwg.org/multipage/input.html#email-state-(type=email)
-	const regExp = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+	// Equivalent to is_email()
+	// https://developer.wordpress.org/reference/functions/is_email/
+	const isValidEmail = text => {
+		text = text.trim();
 
-	if ( ! values.every( text => regExp.test( text.trim() ) ) ) {
+		if ( text.length < 6 ) {
+			return false;
+		}
+
+		if ( text.indexOf( '@', 1 ) === -1 ) {
+			return false;
+		}
+
+		if ( text.indexOf( '@' ) !== text.lastIndexOf( '@' ) ) {
+			return false;
+		}
+
+		const [ local, domain ] = text.split( '@', 2 );
+
+		if ( ! /^[a-zA-Z0-9!#$%&\'*+\/=?^_`{|}~\.-]+$/.test( local ) ) {
+			return false;
+		}
+
+		if ( /\.{2,}/.test( domain ) ) {
+			return false;
+		}
+
+		if ( /(?:^[ \t\n\r\0\x0B.]|[ \t\n\r\0\x0B.]$)/.test( domain ) ) {
+			return false;
+		}
+
+		const subs = domain.split( '.' );
+
+		if ( subs.length < 2 ) {
+			return false;
+		}
+
+		for ( const sub of subs ) {
+			if ( /(?:^[ \t\n\r\0\x0B-]|[ \t\n\r\0\x0B-]$)/.test( sub ) ) {
+				return false;
+			}
+
+			if ( ! /^[a-z0-9-]+$/i.test( sub ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+
+	if ( ! values.every( isValidEmail ) ) {
 		throw new ValidationError( this );
 	}
 };
