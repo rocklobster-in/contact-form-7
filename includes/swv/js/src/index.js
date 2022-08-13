8efa381a7773f177502e441e983d28cc758c2114
Takayuki Miyoshi <takayukister@gmail.com>
2022-08-13 17:46:43 +0900

    Filter rules within swv.validate()
    #902

--- includes/swv/js/src/index.js
+++ includes/swv/js/src/index.js
@@ -3,7 +3,31 @@ import FormDataTree from '@takayukister/form-data-tree';
 import * as validators from './rules';
 import { ValidationError } from './error';
 
-const validate = ( rules, formData ) => {
+const validate = ( schema, formData, options = {} ) => {
+	const { targetFields } = options;
+
+	const rules = ( schema.rules ?? [] ).filter(
+		( { rule, ...properties } ) => {
+			if ( 'function' !== typeof validators[ rule ] ) {
+				return false;
+			}
+
+			if ( 'function' === typeof validators[ rule ].matches ) {
+				return validators[ rule ].matches( properties, options );
+			}
+
+			if ( Array.isArray( targetFields ) && targetFields.length ) {
+				return targetFields.includes( properties.field );
+			}
+
+			return true;
+		}
+	);
+
+	if ( ! rules.length ) { // There is no rule to validate.
+		return new Map();
+	}
+
 	const formDataTree = new FormDataTree( formData );
 
 	const result = rules.reduce( ( prev, current ) => {
@@ -14,10 +38,10 @@ const validate = ( rules, formData ) => {
 		}
 
 		try {
-			validators[rule].call( { rule, ...properties }, formDataTree );
+			validators[ rule ].call( { rule, ...properties }, formDataTree );
 		} catch ( error ) {
 			if ( error instanceof ValidationError ) {
-				return prev.set( properties.field, { error: error.error } );
+				return prev.set( properties.field, error );
 			}
 		}
 
