d0dc0567920f5694e4a2b3cce31591a2b0c16629
Takayuki Miyoshi <takayukister@gmail.com>
2022-09-24 14:30:26 +0900

    Create reflection form-tag type
    #986

---
+++ modules/reflection.php
@@ -0,0 +1,27 @@
+<?php
+
+add_action( 'wpcf7_init', 'wpcf7_add_form_tag_reflection', 10, 0 );
+
+function wpcf7_add_form_tag_reflection() {
+	wpcf7_add_form_tag( 'reflection',
+		'wpcf7_reflection_form_tag_handler',
+		array(
+			'name-attr' => true,
+		)
+	);
+}
+
+function wpcf7_reflection_form_tag_handler( $tag ) {
+	if ( empty( $tag->name ) ) {
+		return '';
+	}
+
+	$html = sprintf(
+		'<span %1$s></span>',
+		wpcf7_format_atts( array(
+			'data-reflection-of' => $tag->name,
+		) )
+	);
+
+	return $html;
+}
