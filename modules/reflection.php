3d1b4cbe49145ff515eaa92a502d953a342f2156
Takayuki Miyoshi <takayukister@gmail.com>
2022-11-01 18:18:04 +0900

    Reflection output for non-Ajax submissions
    #986

--- modules/reflection.php
+++ modules/reflection.php
@@ -18,15 +18,35 @@ function wpcf7_reflection_form_tag_handler( $tag ) {
 		return '';
 	}
 
+	$content = '';
+
+	$values = (array) wpcf7_get_hangover( $tag->name );
+
+	if ( $values and ! wpcf7_get_validation_error( $tag->name ) ) {
+		$values = array_map(
+			function ( $val ) use ( $tag ) {
+				return sprintf(
+					'<output name="%1$s">%2$s</output>',
+					esc_attr( $tag->name ),
+					esc_html( $val )
+				);
+			},
+			$values
+		);
+
+		$content = implode( '', $values );
+	}
+
 	$html = sprintf(
-		'<fieldset %1$s></fieldset>',
+		'<fieldset %1$s>%2$s</fieldset>',
 		wpcf7_format_atts( array(
 			'data-reflection-of' => $tag->name,
 			'class' => $tag->get_class_option(
 				wpcf7_form_controls_class( $tag->type )
 			),
 			'id' => $tag->get_id_option(),
-		) )
+		) ),
+		$content
 	);
 
 	return $html;
