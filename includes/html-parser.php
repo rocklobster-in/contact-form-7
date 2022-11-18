de0d684aad52af3efe2a2e4f312c9b5da67292dc
Takayuki Miyoshi <takayukister@gmail.com>
2022-11-18 18:50:15 +0900

    Update html-parser.php
    #1068

--- includes/html-parser.php
+++ includes/html-parser.php
@@ -11,44 +11,86 @@ class WPCF7_HTMLParser {
 	protected $stack = '';
 	protected $mode = 0;
 	protected $position = 0;
+	protected $callback = null;
 
-	public function __construct( string $input ) {
+	public function __construct( string $input, callable $callback ) {
 		$this->input = $input;
+		$this->callback = $callback;
 	}
 
 	public function parse() {
 		$input_bytelength = strlen( $this->input );
 
 		while ( $this->position < $input_bytelength ) {
-			$this->stack .= $this->input[$this->position];
+			$current_byte = $this->input[$this->position];
 
-			if ( $this->mode === self::text ) {
+			if ( '<' === $current_byte ) {
+				$this->trigger();
+				$this->stack = '<';
+				$this->mode = self::opening_tag;
+				$this->position++;
+				continue;
+			}
+
+			$this->stack .= $current_byte;
+
+			if ( $this->mode === self::opening_tag ) {
 				if ( str_starts_with( $this->stack, '<!--' ) ) {
 					$this->mode = self::comment;
 				} elseif ( str_starts_with( $this->stack, '</' ) ) {
 					$this->mode = self::closing_tag;
-				} elseif ( str_starts_with( $this->stack, '<' ) ) {
-					$this->mode = self::opening_tag;
 				}
-			} elseif ( $this->mode === self::opening_tag ) {
+			}
+
+			if ( $this->mode === self::opening_tag ) {
 				if ( str_ends_with( $this->stack, '>' ) ) {
-					$this->mode = self::text;
+					$this->trigger();
 					$this->stack = '';
+					$this->mode = self::text;
+					$this->position++;
+					continue;
 				}
-			} elseif ( $this->mode === self::closing_tag ) {
+			}
+
+			if ( $this->mode === self::closing_tag ) {
 				if ( str_ends_with( $this->stack, '>' ) ) {
-					$this->mode = self::text;
+					$this->trigger();
 					$this->stack = '';
+					$this->mode = self::text;
+					$this->position++;
+					continue;
 				}
-			} elseif ( $this->mode === self::comment ) {
+			}
+
+			if ( $this->mode === self::comment ) {
 				if ( str_ends_with( $this->stack, '-->' ) ) {
-					$this->mode = self::text;
+					$this->trigger();
 					$this->stack = '';
+					$this->mode = self::text;
+					$this->position++;
+					continue;
 				}
+			}
+
+			if ( 1 === $input_bytelength - $this->position ) { // End of string.
+				$this->trigger();
+				break;
 			}
 
 			$this->position++;
 		}
+	}
+
+	protected function trigger() {
+		if ( '' === $this->stack ) {
+			return;
+		}
+
+		call_user_func( $this->callback, array(
+			'position' => $this->position - strlen( $this->stack ),
+			'type' => $this->mode,
+			'content' => $this->stack,
+		) );
 	}
 
 }
