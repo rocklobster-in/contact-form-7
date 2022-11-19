839c870b09363398da14c9dda5e4309249c0b814
Takayuki Miyoshi <takayukister@gmail.com>
2022-11-19 17:58:03 +0900

    Use generator
    #1068
    
    Much simpler than before.

--- includes/html-parser.php
+++ includes/html-parser.php
@@ -1,96 +1,73 @@
 <?php
 
-class WPCF7_HTMLParser {
+function wpcf7_parse_html( string $input ) {
+	$input_bytelength = strlen( $input );
+	$position = 0;
 
-	const text = 0;
-	const opening_tag = 1;
-	const closing_tag = 2;
-	const comment = 3;
+	while ( $position < $input_bytelength ) {
+		$next_lt = strpos( $input, '<', $position );
 
-	protected $input = '';
-	protected $stack = '';
-	protected $mode = 0;
-	protected $position = 0;
-	protected $callback = null;
+		if ( false === $next_lt ) {
+			yield array(
+				'position' => $position,
+				'type' => 'text',
+				'chunk' => substr( $input, $position ),
+			);
 
-	public function __construct( string $input, callable $callback ) {
-		$this->input = $input;
-		$this->callback = $callback;
-	}
+			break;
+		}
 
-	public function parse() {
-		$input_bytelength = strlen( $this->input );
+		$text_node = substr( $input, $position, $next_lt - $position );
 
-		while ( $this->position < $input_bytelength ) {
-			$current_byte = $this->input[$this->position];
+		if ( '' !== $text_node ) {
+			yield array(
+				'position' => $position,
+				'type' => 'text',
+				'chunk' => $text_node,
+			);
+		}
 
-			if ( '<' === $current_byte ) {
-				$this->trigger();
-				$this->stack = '<';
-				$this->mode = self::opening_tag;
-				$this->position++;
+		if ( '<!--' === substr( $input, $next_lt, 4 ) ) { // Comment
+			$counterpart = strpos( $input, '-->', $next_lt );
+
+			if ( false === $counterpart ) {
+				yield array(
+					'position' => $next_lt,
+					'type' => 'comment',
+					'chunk' => substr( $input, $next_lt ),
+				);
+
+				break;
+			} else {
+				yield array(
+					'position' => $next_lt,
+					'type' => 'comment',
+					'chunk' => substr( $input, $next_lt, $counterpart + 3 - $next_lt ),
+				);
+
+				$position = $counterpart + 3;
 				continue;
 			}
-
-			$this->stack .= $current_byte;
-
-			if ( 1 === $input_bytelength - $this->position ) { // End of string.
-				$this->trigger();
-				break;
-			}
-
-			if ( $this->mode === self::opening_tag ) {
-				if ( str_starts_with( $this->stack, '<!--' ) ) {
-					$this->mode = self::comment;
-				} elseif ( str_starts_with( $this->stack, '</' ) ) {
-					$this->mode = self::closing_tag;
-				}
-			}
-
-			if ( $this->mode === self::opening_tag ) {
-				if ( str_ends_with( $this->stack, '>' ) ) {
-					$this->trigger();
-					$this->stack = '';
-					$this->mode = self::text;
-					$this->position++;
-					continue;
-				}
-			}
-
-			if ( $this->mode === self::closing_tag ) {
-				if ( str_ends_with( $this->stack, '>' ) ) {
-					$this->trigger();
-					$this->stack = '';
-					$this->mode = self::text;
-					$this->position++;
-					continue;
-				}
-			}
-
-			if ( $this->mode === self::comment ) {
-				if ( str_ends_with( $this->stack, '-->' ) ) {
-					$this->trigger();
-					$this->stack = '';
-					$this->mode = self::text;
-					$this->position++;
-					continue;
-				}
-			}
-
-			$this->position++;
-		}
-	}
-
-	protected function trigger() {
-		if ( '' === $this->stack ) {
-			return;
 		}
 
-		call_user_func( $this->callback, array(
-			'position' => $this->position - strlen( $this->stack ),
-			'type' => $this->mode,
-			'content' => $this->stack,
-		) );
-	}
+		if ( preg_match( '/^<(\/?)[a-z].*?>/i', $input, $matches, 0, $next_lt ) ) {
+			yield array(
+				'position' => $next_lt,
+				'type' => ( '/' === $matches[1] ) ? 'closing_tag' : 'opening_tag',
+				'chunk' => $matches[0],
+			);
 
+			$position = $next_lt + strlen( $matches[0] );
+			continue;
+		}
+
+		// Floating '<', not component of a tag or comment.
+		yield array(
+			'position' => $next_lt,
+			'type' => 'text',
+			'chunk' => '<',
+		);
+
+		$position = $next_lt + 1;
+	}
 }
