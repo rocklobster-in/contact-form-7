399a1e7d3f045eabcc26ec6f10f6c2c27f18ef27
Takayuki Miyoshi <takayukister@gmail.com>
2022-11-24 17:56:11 +0900

    Update html-formatter.php
    #1068

--- includes/html-formatter.php
+++ includes/html-formatter.php
@@ -94,7 +94,13 @@ class WPCF7_HTMLFormatter {
 		}
 
 		// Remove lines only with whitespaces.
-		$content = preg_replace( '/\n[\t ]+\n/', "\n\n", $content );
+		$content = preg_replace( '/\n\s*\n/', "\n\n", $content );
+
+		$content = trim( $content );
+
+		if ( '' === $content ) {
+			return;
+		}
 
 		if ( $this->is_inside( self::p_child_elements ) ) {
 			if ( $this->options['auto_br'] ) {
