eedfe65c5689e71c8f5bae2753b76eb22319c4f5
Takayuki Miyoshi <takayukister@gmail.com>
2022-12-28 13:10:02 +0900

    Prepare for 5.7.2 release
    #1126

--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -7,10 +7,10 @@ Author: Takayuki Miyoshi
 Author URI: https://ideasilo.wordpress.com/
 Text Domain: contact-form-7
 Domain Path: /languages/
-Version: 5.7.1
+Version: 5.7.2
 */
 
-define( 'WPCF7_VERSION', '5.7.1' );
+define( 'WPCF7_VERSION', '5.7.2' );
 
 define( 'WPCF7_REQUIRED_WP_VERSION', '6.0' );
 
