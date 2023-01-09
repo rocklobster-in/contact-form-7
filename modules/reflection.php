07570954ea6e9140a8b43fd07a0f793158a355dc
Takayuki Miyoshi <takayukister@gmail.com>
2023-01-09 17:19:50 +0900

    Put an empty <output> when there is no value set
    #1134

--- modules/reflection.php
+++ modules/reflection.php
@@ -18,27 +18,29 @@ function wpcf7_reflection_form_tag_handler( $tag ) {
 		return '';
 	}
 
-	$content = '';
+	$values = $tag->values ? $tag->values : array( '' );
 
-	$values = (array) wpcf7_get_hangover(
-		$tag->name,
-		(string) reset( $tag->values )
-	);
+	if ( ! wpcf7_get_validation_error( $tag->name ) ) {
+		$hangover = array_filter( (array) wpcf7_get_hangover( $tag->name ) );
 
-	if ( $values and ! wpcf7_get_validation_error( $tag->name ) ) {
-		$values = array_map(
-			function ( $val ) use ( $tag ) {
-				return sprintf(
-					'<output name="%1$s">%2$s</output>',
-					esc_attr( $tag->name ),
-					esc_html( $val )
-				);
-			},
-			$values
-		);
-
-		$content = implode( '', $values );
+		if ( $hangover ) {
+			$values = $hangover;
+		}
 	}
+
+	$content = array_reduce(
+		$values,
+		function ( $carry, $item ) use ( $tag ) {
+			$output_tag = sprintf(
+				'<output name="%1$s">%2$s</output>',
+				esc_attr( $tag->name ),
+				$item ? esc_html( $item ) : '&nbsp;'
+			);
+
+			return $carry . $output_tag;
+		},
+		''
+	);
 
 	$html = sprintf(
 		'<fieldset %1$s>%2$s</fieldset>',
