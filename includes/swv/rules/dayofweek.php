f543cd779018c3b7f5f811381aed763486077e6d
Takayuki Miyoshi <takayukister@gmail.com>
2023-06-08 16:59:13 +0900

    SWV: Add rule type for day of the week
    #1244

---
+++ includes/swv/rules/dayofweek.php
@@ -0,0 +1,62 @@
+<?php
+
+class WPCF7_SWV_DayofweekRule extends WPCF7_SWV_Rule {
+
+	const rule_name = 'dayofweek';
+
+	public function matches( $context ) {
+		if ( false === parent::matches( $context ) ) {
+			return false;
+		}
+
+		if ( empty( $context['text'] ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public function validate( $context ) {
+		$field = $this->get_property( 'field' );
+
+		$input = isset( $_POST[$field] ) ? $_POST[$field] : '';
+
+		$input = wpcf7_array_flatten( $input );
+		$input = wpcf7_exclude_blank( $input );
+
+		$acceptable_values = (array) $this->get_property( 'accept' );
+		$acceptable_values = array_map( 'intval', $acceptable_values );
+		$acceptable_values = array_filter( $acceptable_values );
+		$acceptable_values = array_unique( $acceptable_values );
+
+		foreach ( $input as $i ) {
+			if ( ! wpcf7_is_date( $i ) ) {
+				return new WP_Error( 'wpcf7_invalid_dayofweek',
+					$this->get_property( 'error' )
+				);
+			}
+
+			$datetime = date_create( $i, wp_timezone() );
+
+			if ( false === $datetime ) {
+				return new WP_Error( 'wpcf7_invalid_dayofweek',
+					$this->get_property( 'error' )
+				);
+			}
+
+			$dow = (int) $datetime->format( 'N' );
+
+			if ( ! in_array( $dow, $acceptable_values, true ) ) {
+				return new WP_Error( 'wpcf7_invalid_dayofweek',
+					$this->get_property( 'error' )
+				);
+			}
+		}
+
+		return true;
+	}
+
+	public function to_array() {
+		return array( 'rule' => self::rule_name ) + (array) $this->properties;
+	}
+}
--- includes/swv/swv.php
+++ includes/swv/swv.php
@@ -21,6 +21,7 @@ function wpcf7_swv_available_rules() {
 		'date' => 'WPCF7_SWV_DateRule',
 		'file' => 'WPCF7_SWV_FileRule',
 		'enum' => 'WPCF7_SWV_EnumRule',
+		'dayofweek' => 'WPCF7_SWV_DayofweekRule',
 		'minitems' => 'WPCF7_SWV_MinItemsRule',
 		'maxitems' => 'WPCF7_SWV_MaxItemsRule',
 		'minlength' => 'WPCF7_SWV_MinLengthRule',
