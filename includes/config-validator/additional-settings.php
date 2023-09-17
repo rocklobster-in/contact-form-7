2e9b90b34eecba826502300e85858d1b32cdebde
Takayuki Miyoshi <takayukister@gmail.com>
2023-09-17 17:15:13 +0900

    Define $section
    #1290

--- includes/config-validator/additional-settings.php
+++ includes/config-validator/additional-settings.php
@@ -6,6 +6,7 @@ trait WPCF7_ConfigValidator_AdditionalSettings {
 	 * Runs error detection for the additional settings section.
 	 */
 	public function validate_additional_settings() {
+		$section = 'additional_settings.body';
 
 		if ( $this->supports( 'deprecated_settings' ) ) {
 			$deprecated_settings_used =
@@ -13,14 +14,13 @@ trait WPCF7_ConfigValidator_AdditionalSettings {
 				$this->contact_form->additional_setting( 'on_submit' );
 
 			if ( $deprecated_settings_used ) {
-				$this->add_error( 'additional_settings.body',
-					'deprecated_settings',
+				$this->add_error( $section, 'deprecated_settings',
 					array(
 						'message' => __( "Deprecated settings are used.", 'contact-form-7' ),
 					)
 				);
 			} else {
-				$this->remove_error( 'additional_settings.body', 'deprecated_settings' );
+				$this->remove_error( $section, 'deprecated_settings' );
 			}
 		}
 	}
