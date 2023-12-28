68303ab47a24907e9364e227c9a3a104650f4188
Takayuki Miyoshi <takayukister@gmail.com>
2023-12-28 17:56:58 +0900

    Introduce the $options arg to wpcf7_autop_or_not
    Fixes #1340

--- includes/functions.php
+++ includes/functions.php
@@ -212,8 +212,12 @@ function wpcf7_validate_configuration() {
 /**
  * Returns true if wpcf7_autop() is applied.
  */
-function wpcf7_autop_or_not() {
-	return (bool) apply_filters( 'wpcf7_autop_or_not', WPCF7_AUTOP );
+function wpcf7_autop_or_not( $options = '' ) {
+	$options = wp_parse_args( $options, array(
+		'for' => 'form',
+	) );
+
+	return (bool) apply_filters( 'wpcf7_autop_or_not', WPCF7_AUTOP, $options );
 }
 
 
