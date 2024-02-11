3cdc59d7482b8b879370619fbc767dd8d6d277fb
Takayuki Miyoshi <takayukister@gmail.com>
2024-02-11 17:23:50 +0900

    Update recaptcha.php
    #1368

--- modules/recaptcha/recaptcha.php
+++ modules/recaptcha/recaptcha.php
@@ -135,8 +135,7 @@ function wpcf7_recaptcha_verify_response( $spam, $submission ) {
 		return $spam;
 	}
 
-	$token = isset( $_POST['_wpcf7_recaptcha_response'] )
-		? trim( $_POST['_wpcf7_recaptcha_response'] ) : '';
+	$token = trim( $_POST['_wpcf7_recaptcha_response'] ?? '' );
 
 	if ( $service->verify( $token ) ) { // Human
 		$spam = false;
