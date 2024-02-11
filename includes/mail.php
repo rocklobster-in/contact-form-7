3712d26b6c5a2c0d78e7a0f1347b5b3b544edf45
Takayuki Miyoshi <takayukister@gmail.com>
2024-02-11 16:53:54 +0900

    Update mail.php
    #1368

--- includes/mail.php
+++ includes/mail.php
@@ -552,9 +552,7 @@ class WPCF7_MailTaggedText {
 			: null;
 
 		if ( $mail_tag->get_option( 'do_not_heat' ) ) {
-			$submitted = isset( $_POST[$field_name] )
-				? wp_unslash( $_POST[$field_name] )
-				: '';
+			$submitted = wp_unslash( $_POST[$field_name] ?? '' );
 		}
 
 		$replaced = $submitted;
