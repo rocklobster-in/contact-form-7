ff6e66194554acfe72e41509a7cc649fa4241127
Takayuki Miyoshi <takayukister@gmail.com>
2024-07-19 18:45:45 +0900

    Start 6.0 development

--- wp-contact-form-7.php
+++ wp-contact-form-7.php
@@ -7,14 +7,14 @@
  * Author URI: https://ideasilo.wordpress.com/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Version: 5.9.7
- * Requires at least: 6.3
+ * Version: 6.0-dev
+ * Requires at least: 6.5
  * Requires PHP: 7.4
  */
 
-define( 'WPCF7_VERSION', '5.9.7' );
+define( 'WPCF7_VERSION', '6.0-dev' );
 
-define( 'WPCF7_REQUIRED_WP_VERSION', '6.3' );
+define( 'WPCF7_REQUIRED_WP_VERSION', '6.5' );
 
 define( 'WPCF7_TEXT_DOMAIN', 'contact-form-7' );
 
