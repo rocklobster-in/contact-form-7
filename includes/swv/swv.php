c9252bd1f9e62a681468a99f9bde6457a151aa1d
Takayuki Miyoshi <takayukister@gmail.com>
2024-07-24 18:10:56 +0900

    Move stepnumber after min/max rules
    #1250

--- includes/swv/swv.php
+++ includes/swv/swv.php
@@ -22,7 +22,6 @@ function wpcf7_swv_available_rules() {
 		'date' => 'Contactable\SWV\DateRule',
 		'time' => 'Contactable\SWV\TimeRule',
 		'file' => 'Contactable\SWV\FileRule',
-		'stepnumber' => 'Contactable\SWV\StepNumberRule',
 		'enum' => 'Contactable\SWV\EnumRule',
 		'dayofweek' => 'Contactable\SWV\DayofweekRule',
 		'minitems' => 'Contactable\SWV\MinItemsRule',
@@ -35,6 +34,7 @@ function wpcf7_swv_available_rules() {
 		'maxdate' => 'Contactable\SWV\MaxDateRule',
 		'minfilesize' => 'Contactable\SWV\MinFileSizeRule',
 		'maxfilesize' => 'Contactable\SWV\MaxFileSizeRule',
+		'stepnumber' => 'Contactable\SWV\StepNumberRule',
 		'all' => 'Contactable\SWV\AllRule',
 		'any' => 'Contactable\SWV\AnyRule',
 	);
