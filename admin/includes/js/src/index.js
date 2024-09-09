8df45c12bf3687339fad3aa7b18cdd43eab0e7f5
Takayuki Miyoshi <takayukister@gmail.com>
2024-09-09 16:52:25 +0900

    beforeunload
    #1464

--- admin/includes/js/src/index.js
+++ admin/includes/js/src/index.js
@@ -2,6 +2,7 @@ import { init as initTabs } from './tabs';
 import { init as initConfigFields } from './config-errors';
 import { init as initWelcomePanel } from './welcome-panel';
 import { init as initTagGenerator } from './tag-generator';
+import { init as initBeforeUnload } from './before-unload';
 import { toggleFieldset } from './utils';
 
 
@@ -10,6 +11,7 @@ document.addEventListener( 'DOMContentLoaded', event => {
 	initConfigFields();
 	initWelcomePanel();
 	initTagGenerator();
+	initBeforeUnload();
 
 	const titleField = document.querySelector( 'input#title' );
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -7,40 +7,8 @@
 	}
 
 	$( function() {
-		$( window ).on( 'beforeunload', function( event ) {
-			var changed = false;
-
-			$( '#wpcf7-admin-form-element :input[type!="hidden"]' ).each( function() {
-				if ( $( this ).is( ':checkbox, :radio' ) ) {
-					if ( this.defaultChecked != $( this ).is( ':checked' ) ) {
-						changed = true;
-					}
-				} else if ( $( this ).is( 'select' ) ) {
-					$( this ).find( 'option' ).each( function() {
-						if ( this.defaultSelected != $( this ).is( ':selected' ) ) {
-							changed = true;
-						}
-					} );
-				} else if ( $( this ).is( 'button' ) ) { // Tag generator buttons
-					// Do nothing
-				} else {
-					if ( this.defaultValue != $( this ).val() ) {
-						changed = true;
-					}
-				}
-			} );
-
-			if ( changed ) {
-				event.returnValue = wpcf7.saveAlert;
-				return wpcf7.saveAlert;
-			}
-		} );
 
 		$( '#wpcf7-admin-form-element' ).submit( function() {
-			if ( 'copy' != this.action.value ) {
-				$( window ).off( 'beforeunload' );
-			}
-
 			if ( 'save' == this.action.value ) {
 				$( '#publishing-action .spinner' ).addClass( 'is-active' );
 			}
