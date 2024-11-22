50fd5e05b0c51a7470946cbd62d15bf9c08d3fbd
Takayuki Miyoshi <takayukister@gmail.com>
2024-11-22 16:59:12 +0900

    Normalize newlines before enum validation
    Fixes #1512

--- includes/swv/php/rules/enum.php
+++ includes/swv/php/rules/enum.php
@@ -25,7 +25,16 @@ class EnumRule extends Rule {
 		$input = wpcf7_exclude_blank( $input );
 
 		$acceptable_values = (array) $this->get_property( 'accept' );
-		$acceptable_values = array_map( 'strval', $acceptable_values );
+
+		$acceptable_values = array_map(
+			static function ( $val ) {
+				$val = strval( $val );
+				$val = wpcf7_normalize_newline( $val );
+				return $val;
+			},
+			$acceptable_values
+		);
+
 		$acceptable_values = array_unique( $acceptable_values );
 
 		$acceptable_values = array_filter( $acceptable_values,
@@ -35,6 +44,8 @@ class EnumRule extends Rule {
 		);
 
 		foreach ( $input as $i ) {
+			$i = wpcf7_normalize_newline( $i );
+
 			if ( ! in_array( $i, $acceptable_values, true ) ) {
 				return $this->create_error();
 			}
