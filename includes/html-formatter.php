d43611e3e7eaac2452b6ea9a7d9a35f211b47280
Takayuki Miyoshi <takayukister@gmail.com>
2025-03-22 17:46:15 +0900

    Define options
    #1570

--- includes/html-formatter.php
+++ includes/html-formatter.php
@@ -99,6 +99,8 @@ class WPCF7_HTMLFormatter {
 		$this->options = wp_parse_args( $options, array(
 			'auto_br' => true,
 			'auto_indent' => true,
+			'auto_paragraph' => true,
+			'auto_parent' => true,
 		) );
 	}
 
