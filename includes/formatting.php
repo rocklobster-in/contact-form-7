f1a954d9699c5ce989e99d53c7a1df0115e2f2ed
Takayuki Miyoshi <takayukister@gmail.com>
2025-04-05 16:35:52 +0900

    Expand attribute allowance
    #1573

--- includes/formatting.php
+++ includes/formatting.php
@@ -396,6 +396,7 @@ function wpcf7_kses_allowed_html( $context = 'form' ) {
 			'input' => array(
 				'accept' => true,
 				'alt' => true,
+				'autocomplete' => true,
 				'capture' => true,
 				'checked' => true,
 				'disabled' => true,
@@ -444,12 +445,14 @@ function wpcf7_kses_allowed_html( $context = 'form' ) {
 				'value' => true,
 			),
 			'select' => array(
+				'autocomplete' => true,
 				'disabled' => true,
 				'multiple' => true,
 				'name' => true,
 				'size' => true,
 			),
 			'textarea' => array(
+				'autocomplete' => true,
 				'cols' => true,
 				'disabled' => true,
 				'maxlength' => true,
@@ -458,7 +461,6 @@ function wpcf7_kses_allowed_html( $context = 'form' ) {
 				'placeholder' => true,
 				'readonly' => true,
 				'rows' => true,
-				'spellcheck' => true,
 				'wrap' => true,
 			),
 		);
@@ -484,6 +486,7 @@ function wpcf7_kses_allowed_html( $context = 'form' ) {
 					'id' => true,
 					'inputmode' => true,
 					'role' => true,
+					'spellcheck' => true,
 					'style' => true,
 					'tabindex' => true,
 					'title' => true,
