29677c51af522ae9ae8e28e7bb8c2b318cd3a1e2
Takayuki Miyoshi <takayukister@gmail.com>
2025-04-13 17:26:58 +0900

    Update service.php
    #1573

--- modules/akismet/service.php
+++ modules/akismet/service.php
@@ -38,33 +38,49 @@ class WPCF7_Akismet extends WPCF7_Service {
 
 
 	public function link() {
-		echo wpcf7_link(
+		echo wp_kses_data( wpcf7_link(
 			'https://akismet.com/',
 			'akismet.com'
-		);
+		) );
 	}
 
 
 	public function display( $action = '' ) {
-		echo sprintf(
-			'<p>%s</p>',
-			esc_html( __( "CAPTCHAs are designed to distinguish spambots from humans, and are therefore helpless against human spammers. In contrast to CAPTCHAs, Akismet checks form submissions against the global database of spam; this means Akismet is a comprehensive solution against spam. This is why we consider Akismet to be the centerpiece of the spam prevention strategy.", 'contact-form-7' ) )
+		$formatter = new WPCF7_HTMLFormatter();
+
+		$formatter->append_start_tag( 'p' );
+
+		$formatter->append_preformatted(
+			esc_html( __( 'CAPTCHAs are designed to distinguish spambots from humans, and are therefore helpless against human spammers. In contrast to CAPTCHAs, Akismet checks form submissions against the global database of spam; this means Akismet is a comprehensive solution against spam. This is why we consider Akismet to be the centerpiece of the spam prevention strategy.', 'contact-form-7' ) )
 		);
 
-		echo sprintf(
-			'<p><strong>%s</strong></p>',
+		$formatter->end_tag( 'p' );
+
+		$formatter->append_start_tag( 'p' );
+		$formatter->append_start_tag( 'strong' );
+
+		$formatter->append_preformatted(
 			wpcf7_link(
 				__( 'https://contactform7.com/spam-filtering-with-akismet/', 'contact-form-7' ),
 				__( 'Spam filtering with Akismet', 'contact-form-7' )
 			)
 		);
 
+		$formatter->end_tag( 'p' );
+
 		if ( $this->is_active() ) {
-			echo sprintf(
-				'<p class="dashicons-before dashicons-yes">%s</p>',
-				esc_html( __( "Akismet is active on this site.", 'contact-form-7' ) )
+			$formatter->append_start_tag( 'p', array(
+				'class' => 'dashicons-before dashicons-yes',
+			) );
+
+			$formatter->append_preformatted(
+				esc_html( __( 'Akismet is active on this site.', 'contact-form-7' ) )
 			);
+
+			$formatter->end_tag( 'p' );
 		}
+
+		$formatter->print();
 	}
 
 }
