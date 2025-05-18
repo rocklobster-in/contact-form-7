26d35312d9d2b65a3b0e127b34c7ffc1fdb77c5c
Takayuki Miyoshi <takayukister@gmail.com>
2025-05-18 18:07:38 +0900

    Use wpcf7_strip_whitespaces()
    #1622

--- includes/pipe.php
+++ includes/pipe.php
@@ -20,10 +20,10 @@ class WPCF7_Pipe {
 		$pipe_pos = strpos( $text, '|' );
 
 		if ( false === $pipe_pos ) {
-			$this->before = $this->after = trim( $text );
+			$this->before = $this->after = wpcf7_strip_whitespaces( $text );
 		} else {
-			$this->before = trim( substr( $text, 0, $pipe_pos ) );
-			$this->after = trim( substr( $text, $pipe_pos + 1 ) );
+			$this->before = wpcf7_strip_whitespaces( substr( $text, 0, $pipe_pos ) );
+			$this->after = wpcf7_strip_whitespaces( substr( $text, $pipe_pos + 1 ) );
 		}
 	}
 }
