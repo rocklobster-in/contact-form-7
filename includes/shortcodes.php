6279e6a228901dc48f35acb93ac83bd9836c23f8
Takayuki Miyoshi <takayukister@gmail.com>
2025-06-22 17:05:47 +0900

    Update shortcodes.php
    #23

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -38,7 +38,8 @@ class WPCF7_ShortcodeManager {
 
 	public static function get_instance() {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::get_instance' );
+			'WPCF7_FormTagsManager::get_instance'
+		);
 
 		self::$form_tags_manager = WPCF7_FormTagsManager::get_instance();
 		return new self();
@@ -46,35 +47,40 @@ class WPCF7_ShortcodeManager {
 
 	public function get_scanned_tags() {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::get_scanned_tags' );
+			'WPCF7_FormTagsManager::get_scanned_tags'
+		);
 
 		return self::$form_tags_manager->get_scanned_tags();
 	}
 
 	public function add_shortcode( $tag, $callback, $has_name = false ) {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::add' );
+			'WPCF7_FormTagsManager::add'
+		);
 
 		return self::$form_tags_manager->add( $tag, $callback, $has_name );
 	}
 
 	public function remove_shortcode( $tag ) {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::remove' );
+			'WPCF7_FormTagsManager::remove'
+		);
 
 		return self::$form_tags_manager->remove( $tag );
 	}
 
 	public function normalize_shortcode( $content ) {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::normalize' );
+			'WPCF7_FormTagsManager::normalize'
+		);
 
 		return self::$form_tags_manager->normalize( $content );
 	}
 
 	public function do_shortcode( $content, $exec = true ) {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::replace_all' );
+			'WPCF7_FormTagsManager::replace_all'
+		);
 
 		if ( $exec ) {
 			return self::$form_tags_manager->replace_all( $content );
@@ -85,7 +91,8 @@ class WPCF7_ShortcodeManager {
 
 	public function scan_shortcode( $content ) {
 		wpcf7_deprecated_function( __METHOD__, '4.6',
-			'WPCF7_FormTagsManager::scan' );
+			'WPCF7_FormTagsManager::scan'
+		);
 
 		return self::$form_tags_manager->scan( $content );
 	}
