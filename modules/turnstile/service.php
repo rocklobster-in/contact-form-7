a23739d4f3a35703e92e7af77b4835f7a5a4cd4a
Takayuki Miyoshi <takayukister@gmail.com>
2025-08-02 13:49:50 +0900

    Update service.php
    #1677

--- modules/turnstile/service.php
+++ modules/turnstile/service.php
@@ -236,14 +236,14 @@ class WPCF7_Turnstile extends WPCF7_Service {
 	public function admin_notice( $message = '' ) {
 		if ( 'invalid' === $message ) {
 			wp_admin_notice(
-				wp_kses_data( __( '<strong>Error:</strong> Invalid key values.', 'contact-form-7' ) ),
+				__( '<strong>Error:</strong> Invalid key values.', 'contact-form-7' ),
 				array( 'type' => 'error' )
 			);
 		}
 
 		if ( 'success' === $message ) {
 			wp_admin_notice(
-				wp_kses_data( __( 'Settings saved.', 'contact-form-7' ) ),
+				__( 'Settings saved.', 'contact-form-7' ),
 				array( 'type' => 'success' )
 			);
 		}
