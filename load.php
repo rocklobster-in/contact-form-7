645d994b00d1dcff693006ecf9740bec6a0a1dc3
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-03 14:20:18 +0900

    Install Composer
    #1723
    
    https://packagist.org/

--- load.php
+++ load.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'vendor/autoload.php';
+
 require_once WPCF7_PLUGIN_DIR . '/includes/l10n.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/capabilities.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/filesystem.php';
