956eb3ba0ce8eb4c0f66dac2cde0d04adae374fb
Takayuki Miyoshi <takayukister@gmail.com>
2026-01-04 16:16:30 +0900

    Update index.js

--- includes/swv/js/src/index.js
+++ includes/swv/js/src/index.js
@@ -1,12 +1,41 @@
 import {
-	validators,
-	validate,
-	registerMiddleware as use
-} from '@contactable/swv';
+	InvalidityException,
+	AbstractRule,
+	CompositeRule,
+	rules,
+} from '@rocklobsterinc/swv';
+
+const availableRules = new Map( [
+	[ 'all', rules.AllRule ],
+	[ 'any', rules.AnyRule ],
+	[ 'date', rules.DateRule ],
+	[ 'dayofweek', rules.DayofweekRule ],
+	[ 'email', rules.EmailRule ],
+	[ 'enum', rules.EnumRule ],
+	[ 'file', rules.FileRule ],
+	[ 'maxdate', rules.MaxDateRule ],
+	[ 'maxfilesize', rules.MaxFilesizeRule ],
+	[ 'maxitems', rules.MaxItemsRule ],
+	[ 'maxlength', rules.MaxLengthRule ],
+	[ 'maxnumber', rules.MaxNumberRule ],
+	[ 'mindate', rules.MinDateRule ],
+	[ 'minfilesize', rules.MinFilesizeRule ],
+	[ 'minitems', rules.MinItemsRule ],
+	[ 'minlength', rules.MinLengthRule ],
+	[ 'minnumber', rules.MinNumberRule ],
+	[ 'number', rules.NumberRule ],
+	[ 'required', rules.RequiredRule ],
+	[ 'requiredfile', rules.RequiredFileRule ],
+	[ 'stepnumber', rules.StepNumberRule ],
+	[ 'tel', rules.TelRule ],
+	[ 'time', rules.TimeRule ],
+	[ 'url', rules.URLRule ],
+] );
 
 window.swv = {
-	validators,
-	validate,
-	use,
+	InvalidityException,
+	AbstractRule,
+	CompositeRule,
+	availableRules,
 	...( window.swv ?? {} ),
 };
