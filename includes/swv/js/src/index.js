2ea5ba3b8d034b28696f0cd55bb919839a6c83bd
Takayuki Miyoshi <takayukister@gmail.com>
2026-01-10 13:12:41 +0900

    validate()

--- includes/swv/js/src/index.js
+++ includes/swv/js/src/index.js
@@ -4,15 +4,43 @@ import {
 	InvalidityException,
 	AbstractRule,
 	CompositeRule,
-	rules as ruleDictionary,
+	rulesDictionary,
 } from '@rocklobsterinc/swv';
+
+
+const validate = ( schema, formData, context ) => {
+	const rules = schema.rules ?? [];
+	const formDataTree = new FormDataTree( formData );
+
+	const result = rules.reduce( ( result, rule ) => {
+		const ruleObj = constructRule( rule );
+
+		if ( ruleObj.matches( context ) ) {
+			try {
+				ruleObj.validate( formDataTree, context );
+			} catch ( error ) {
+				if ( error instanceof Invalidity ) {
+					if ( error.rule.field && ! result.has( error.rule.field ) ) {
+						result.set( error.rule.field, error.message );
+					}
+				} else {
+					throw error;
+				}
+			}
+		}
+
+		return result;
+	}, new Map() );
+
+	return result;
+};
 
 
 const constructRule = properties => {
 	const { ruleName: rule, rules, ...remainingProperties } = properties;
 
-	if ( ruleDictionary.has( ruleName ) ) {
-		const constructor = ruleDictionary.get( ruleName );
+	if ( rulesDictionary.has( ruleName ) ) {
+		const constructor = rulesDictionary.get( ruleName );
 		const ruleObj = new constructor( remainingProperties );
 
 		if ( ruleObj instanceof CompositeRule ) {
@@ -34,7 +62,7 @@ window.swv = {
 	InvalidityException,
 	AbstractRule,
 	CompositeRule,
-	ruleDictionary,
+	rulesDictionary,
 	constructRule,
 	...( window.swv ?? {} ),
 };
