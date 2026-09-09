83a6e1d56278e270abf9fe45c5dad2332a28332c
Takayuki Miyoshi <takayukister@gmail.com>
2026-09-09 18:19:39 +0900

    Remove warning from #misc-publishing-actions
    #1835

--- admin/includes/js/src/config-errors.js
+++ admin/includes/js/src/config-errors.js
@@ -26,7 +26,7 @@ const init = () => {
 
 const update = () => {
 	document.querySelectorAll(
-		'#contact-form-editor .config-error, #misc-publishing-actions .config-error'
+		'#contact-form-editor .config-error'
 	).forEach( error => {
 		error.remove();
 	} );
@@ -90,29 +90,6 @@ const update = () => {
 			panel.prepend( errMsg );
 		}
 	} );
-
-	if ( errorsCountTotal ) {
-		const errMsg = document.createElement( 'div' );
-		errMsg.classList.add( 'misc-pub-section', 'config-error' );
-
-		const howtoLink = document.createElement( 'a' );
-		howtoLink.setAttribute( 'href', wpcf7.configValidator.docUrl );
-		howtoLink.append( __( "How to resolve?", 'contact-form-7' ) );
-
-		errMsg.append(
-			iconInCircle( '!' ),
-			sprintf( _n(
-				'%d configuration error detected.',
-				'%d configuration errors detected.',
-				errorsCountTotal,
-				'contact-form-7'
-			), errorsCountTotal ),
-			document.createElement( 'br' ),
-			howtoLink
-		);
-
-		document.querySelector( '#misc-publishing-actions' )?.append( errMsg );
-	}
 };
 
 
